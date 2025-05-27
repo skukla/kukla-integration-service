eddb07282eae18102d89937a7f9bb2cfea5f8400
Steve Kukla <kukla@adobe.com>
2025-05-27 13:09:24 -0400

    refactor(commerce): Restructure commerce module for better organization
    - Move integration.js to api module\n- Create transform module for product transformations\n- Add commerce module entry point\n- Improve module documentation

---
+++ src/commerce/api/integration.js
@@ -0,0 +1,9 @@
+/**
+ * Adobe Commerce API integration utilities
+ * @module commerce/api/integration
+ */
+
+const { http: { makeRequest, buildHeaders } } = require('../../core');
+
+// Import the file content from src/commerce/integration.js
+// ... existing code ... 
