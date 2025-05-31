d01df3dfc50b502364b5735365287cc6fc1d30b7
Steve Kukla <kukla@adobe.com>
2025-05-31 15:50:24 -0400

    chore: remove deprecated scripts and test files

--- scripts/stop-dev.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Find and kill the dev server process
-pkill -f "aio app dev"
-
-# Find and kill any node processes running on port 9080
-lsof -ti:9080 | xargs kill -9 2>/dev/null
-
-echo "Development server stopped" 
