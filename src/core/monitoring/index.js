ea3f735e9a90b96124fb644fa5a17937287c6823
Steve Kukla <kukla@adobe.com>
2025-05-31 15:48:35 -0400

    refactor: improve core infrastructure components

--- src/core/monitoring/index.js
+++ src/core/monitoring/index.js
@@ -1,16 +1,38 @@
 /**
- * Core monitoring module entry point
+ * Core monitoring module
  * @module core/monitoring
  */
 
-const { createPerformanceMiddleware } = require('./performance');
-const { createErrorResponse, processError } = require('./errors');
+const { ErrorMonitor } = require('./errors');
+const { PerformanceMonitor, MetricTypes } = require('./performance');
+
+/**
+ * Create monitoring middleware
+ * @param {Object} options Monitoring options
+ * @returns {Function} Monitoring middleware
+ */
+function createMonitoringMiddleware(options = {}) {
+  const perfMonitor = new PerformanceMonitor(options.logger, options.performance);
+  const errorMonitor = new ErrorMonitor(options.logger, options.errors);
+
+  return async function monitor(req, res, next) {
+    const operationId = `${req.method} ${req.path}`;
+    perfMonitor.start(operationId);
+
+    try {
+      await next();
+      perfMonitor.end(operationId, { success: true });
+    } catch (error) {
+      errorMonitor.captureError(error, { req });
+      perfMonitor.end(operationId, { success: false, error: error.message });
+      throw error;
+    }
+  };
+}
 
 module.exports = {
-    // Performance monitoring
-    createPerformanceMiddleware,
-    
-    // Error handling
-    createErrorResponse,
-    processError
-}; 
+  PerformanceMonitor,
+  ErrorMonitor,
+  MetricTypes,
+  createMonitoringMiddleware,
+};
