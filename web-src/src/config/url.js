f93400efa2f03c46b80f4f15c580b858740d7017
Steve Kukla <kukla@Steves-MacBook-Pro.local>
2025-06-07 20:06:05 -0400

    cleanup: remove redundant URL and config files

--- web-src/src/config/url.js
+++
@@ -1,8 +0,0 @@
-/**
- * Frontend URL configuration
- * @module web-src/config/url
- */
-
-import runtimeConfig from './generated/runtime.config.js';
-
-export const urlConfig = runtimeConfig.runtime;
