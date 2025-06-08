ed472a4e094beae092267fd7e887f5450c1829b0
Steve Kukla <kukla@adobe.com>
2025-06-07 23:23:04 -0400

    frontend: create UI modules and update imports to new structure

---
+++ web-src/src/js/htmx/index.js
@@ -0,0 +1,7 @@
+/**
+ * HTMX integration module
+ * @module htmx
+ */
+
+// Re-export HTMX setup functionality
+export { initializeHTMX } from './setup.js';
---
+++ web-src/src/js/ui/index.js
@@ -0,0 +1,31 @@
+/**
+ * UI module exports
+ * Re-exports all UI components and utilities
+ * @module ui
+ */
+
+// Modal component
+export {
+  showModal,
+  hideModal,
+  createModal,
+  initializeModal,
+  handleModalContentSwap,
+  handleModalBeforeSwap,
+} from './components/modal/index.js';
+
+// Notifications component
+export {
+  showNotification,
+  clearNotifications,
+  handleSuccessResult,
+  handleErrorResult,
+  handleDeleteResult,
+} from './components/notifications/index.js';
+
+// File browser
+export {
+  initializeFileBrowser,
+  refreshFileList,
+  handleFileOperationResult,
+} from './file-browser/index.js';
